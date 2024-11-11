1dd07dfe358ddce3de7f6bb822fc4e5b90e6674f
allagaymen <a.allag@esi-sba.dz>
2024-11-11 15:21:45 +0100

    Customize Admin .

--- app/app/settings.py
+++ app/app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf_spectacular',
     'core',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -134,6 +135,8 @@ STATIC_ROOT = '/vol/web/static'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,58 @@
+"""
+Test for models .
+
+"""
+from unittest.mock import patch
+
+from decimal import Decimal
+from core import models
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+def create_user(email='test@example.com', password="test123"):
+    "create and return a new user."
+    return get_user_model().objects.create_user(email, password)
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """ test creating user with an email successfully."""
+        email = 'test@example.com'
+        password ='testpass1234'
+        user = get_user_model().objects.create_user(
+            email = email,
+            password = password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+    
+    def test_new_user_email_normalized(self):
+        """ Test email is normalized for new users ."""
+
+        sample_email = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com']
+        ]
+
+        for email, expected in sample_email:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_errors(self):
+        """Test creating new user without an email raises a ValueError. """
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
+
+    def test_create_superuser(self):
+        """Test creating superuser."""
+        User = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123'
+        )
+
+        self.assertTrue(User.is_superuser)
+        self.assertTrue(User.is_staff)
