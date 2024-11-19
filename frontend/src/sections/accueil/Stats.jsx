eae4e125fce2fb51c1915567619b9c4a55fd9815
XeedOus <djezirixeedoussama@gmail.com>
2024-11-19 17:03:25 +0100

    stats component start

---
+++ frontend/src/sections/accueil/Stats.jsx
@@ -0,0 +1,23 @@
+const content = [
+  {
+    id: 1,
+    value: "",
+    title: "Title 1",
+  },
+  {
+    id: 2,
+    value: "",
+    title: "Title 2",
+  },
+  {
+    id: 3,
+    value: "",
+    title: "Title 3",
+  },
+];
+
+function Stats() {
+  return <div></div>;
+}
+
+export default Stats;
